68f0ee06010068a2044f8536cc529adda0a70e69
YusukeNara <rakyyongaming@gmail.com>
2022-07-20 14:08:58 +0900

    数学課題9 振り子

--- Project1/Title.cpp
+++ Project1/Title.cpp
@@ -12,171 +12,50 @@ Title::Title(ISceneChanger *changer) : BaseScene(changer) {
     
     camera->SetViewStatusEyeTargetUp(eye, target, up);
 
-    newObjectSystem = LoadModel_ObjFile("player");
-    ship2 = LoadModel_ObjFile("player");
-    ship3 = LoadModel_ObjFile("player");
-
-    scale1 = { 10.0,10.0,10.0 };
-    rot1 = { 0,0,0 };
-    pos1 = { -100,5,0 };
-
-    scale2 = { 10.0,10.0,10.0 };
-    rot2 = { 0,0,0 };
-    pos2 = { 0,5,100 };
-
-    scale3 = { 10.0,10.0,10.0 };
-    rot3 = { 0,0,0 };
-    pos3 = { 100,5,0 };
-
-    newObjectSystem->SetAffineParam(scale1, rot1, pos1);
-    ship2->SetAffineParam(scale2, rot2, pos2);
-    ship3->SetAffineParam(scale3, rot3, pos3);
-
-    //画像の読み込み
-    tiletex = TexManager::LoadTexture("Resources/white.png");
-
-    tileObject = NY_Object3DManager::Get()->CreateModel_Tile(500, 500, 50, 50, tiletex);
-    tileObject->color = DirectX::XMFLOAT4(1, 1, 1, 1);
-    tileObject->SetAffineParam(RVector3(1, 1, 1), RVector3(90, 0, 0), RVector3(0, 0, 0));
-
-    //スプライトに画像を割り当てる（画像のハンドル、縦幅、横幅）
-    testInstance.Create(tiletex);
-    x1 = 100.0f;
-    x2 = 150.0f;
-    y1 = 100.0f;
-    y2 = 150.0f;
-
-    //パーティクル
-    particle1 = ParticleManager::Create();
-    bomProto = new BomParticle(RVector3(0, 0, 0));
-    //プロトタイプをセット
-    particle1->Prototype_Set(bomProto);
-    //パーティクルのテクスチャ
-    ptex = TexManager::LoadTexture("Resources/effect1.png");
-
-    //プレイヤー
-    pl.Init();
-    //敵
-    enemy.Init(newObjectSystem);
-    //各種行動オブジェクト生成
-
-    //移動の判定ノード
-    firstNode = new BehaviorBaseNode;
-    //判定スクリプト付きオブジェクト
-    MoveJudgeObject* moveJudgeObject = new MoveJudgeObject(&enemy, &pl.pos);
-
-    //行動オブジェクト
-    approachNode      = new BehaviorBaseNode;
-    approachObject    = new ApproachingMoveAct(&enemy, &pl.pos);
-    approachObject->actScriptName = std::string("approach");
-    approachNode->CreateActionNode("approachAction", approachObject, moveJudgeObject);
-    retreatNode       = new BehaviorBaseNode;
-    retreatObject     = new RetreatMoveAct(&enemy, &pl.pos);
-    retreatObject->actScriptName = std::string("retreat");
-    retreatNode->CreateActionNode("retreatAction", retreatObject, moveJudgeObject);
-    waitNode          = new BehaviorBaseNode;
-    waitObject        = new WaitAct(&enemy, &pl.pos);
-    waitObject->actScriptName = std::string("wait");
-    waitNode->CreateActionNode("WaitAction", waitObject, moveJudgeObject);
-
-    //ノード生成
-    firstNode->CreateJudgeNode("firstNode", BehaviorBaseNode::RULE_RANDOM, moveJudgeObject);
-    firstNode->AddjudgeNodeChild(approachNode);
-    firstNode->AddjudgeNodeChild(retreatNode);
-    firstNode->AddjudgeNodeChild(waitNode);
-
-    //ビヘイビア初期化
-    enemyBehaviorTree.Init(firstNode);
-
-    editor.Init(&enemyBehaviorTree);
-    editor.AddEditData_Node(approachNode);
-    editor.AddEditData_Node(retreatNode);
-    editor.AddEditData_Node(waitNode);
-
-    editor.AddEditData_ActScript(approachObject);
-    editor.AddEditData_ActScript(retreatObject);
-    editor.AddEditData_ActScript(waitObject);
+    circleSprite.Create(TexManager::LoadTexture("Resources/blockParticleTex.png"));
 }
 
 //初期化
 void Title::Initialize() {
-
+    speed = 0.0f;
+    x = CIRCLE_LENGHT / 8.0f;
 }
 
 void Title::Finalize()
 {
-    DeleteObject(ship2);
-    DeleteObject(ship3);
-    DeleteObject(newObjectSystem);
-    DeleteObject(tileObject);
 
-    pl.Finalize();
-
-    delete firstNode;
-    delete approachNode;
-    delete approachObject;
-    delete retreatNode;
-    delete retreatObject;
-    delete waitObject;
-    delete waitNode;
 }
 
 //更新
 void Title::Update() {
-    camera->SetViewStatusEyeTargetUp(eye, target, up);
 
-    NY_Object3DManager::Get()->UpdateAllObjects();
+    speed += -MASS * G * sinf(x / ROPE_LENGHT);
+    x += speed;
 
-    if(Input::isKey(DIK_SPACE)){ particle1->Prototype_Add(3, RVector3(0, 10, 0)); }
+    angle = (x / ROPE_LENGHT) + (PI / 2.0f);
 
-    //Object3dのテスト
+    nx = cosf(angle) * ROPE_LENGHT;
+    ny = sinf(angle) * ROPE_LENGHT;
 
-    //速度計算
-
-    //newObjectSystem->SetAffineParamTranslate(pos1);
-    ship2->SetAffineParamTranslate(pos2);
-    ship3->SetAffineParamTranslate(pos3);
-
-    if (Input::isXpadButtonPushTrigger(XPAD_BUTTON_OPTION_L)) {
-        particle1->Prototype_Add(5, pos1);
-    }
-    if (Input::isXpadButtonPushed(XPAD_BUTTON_OPTION_R)) {
-        particle1->Prototype_Add(20, { 0,0,0 });
-    }
-
-    particle1->Prototype_Update();
-
-    pl.Update();
-
-    enemyBehaviorTree.Run();
 }
 
 //描画
 void Title::Draw() {
 
-    //パーティクル描画
-    particle1->Prototype_Draw(ptex);
-
-    //描画1回目
-    //newObjectSystem->DrawObject();
-    ship2->DrawObject();
-    ship3->DrawObject();
-    tileObject->DrawObject();
-
-    pl.Draw();
-
-    enemy.Draw();
+    //描画中心座標
+    float posX = nx + axis_x;
+    float posY = ny + axis_y;
 
     SpriteManager::Get()->SetCommonBeginDraw();
 
-    testInstance.DrawExtendSprite(x1, y1, x2, y2);
-    testInstance.DrawExtendSprite(x1, y1 + 50, x2, y2 + 50);
+    circleSprite.DrawExtendSprite(posX - SIZE, posY - SIZE, posX + SIZE, posY + SIZE);
+    circleSprite.Draw();
 
-    //本描画（実際に描画される）
-    testInstance.Draw();
+    ImguiMgr::Get()->StartDrawImgui("furiko_param", 100, 100);
 
-    editor.EditorDraw();
-    editor.ObjectDataDraw();
-    editor.NodeDataDraw();
+    ImGui::Text("pos : %0.2f %0.2f", posX, posY);
+    ImGui::SliderFloat("ROPE_LENGHT", &ROPE_LENGHT, 60.0f, 300.0f);
+
+    ImguiMgr::Get()->EndDrawImgui();
 
 }
--- Project1/Title.h
+++ Project1/Title.h
@@ -37,92 +37,23 @@ public:
     XMFLOAT3 target = { 0,0,0 };
     XMFLOAT3 up = { 0,1,0 };
 
-    XMFLOAT3 eye2 = { 0,50,0 };
-    XMFLOAT3 target2 = { 0,0,0 };
-    XMFLOAT3 up2 = { 0,1,0 };
+    const float PI              = 3.14f;
+    float ROPE_LENGHT     = 200.0f;
+    const float CIRCLE_LENGHT   = (ROPE_LENGHT * 2 * PI);
+    const float MASS            = 0.3f;
+    const float G               = 9.81f;
 
-    int animationTime;
-    const int ANIM_TIME = 60;
+    const float SIZE = 20.0f;
 
-    //オブジェクト
-    Object3d *obj1;
-    Object3d *obj2;
-    Object3d *gmodel;
-    RVector3 scale = { 5,5,5 };
+    float x;
+    float speed;
+    float angle;
+    float axis_x = 320.0f;
+    float axis_y = 100.0f;
+    float nx, ny;
 
-    //新型Object3d
-    Object3d *newObjectSystem;
-    Object3d* ship2;
-    Object3d* ship3;
-    //上用変換情報
-    RVector3 scale1, rot1, pos1 = RVector3(-50, 250, 0);
-    RVector3 scale2, rot2, pos2;
-    RVector3 scale3, rot3, pos3;
-    //速度格納
-    RVector3 vel = RVector3(0, 0, 0);
-    RVector3 vel2 = RVector3(0, 0, 0);
-    RVector3 vel3 = RVector3(0, 0, 0);
-    RVector3 acc = RVector3(0, 0, 0);
-    RVector3 acc2 = RVector3(0, 0, 0);
-    RVector3 acc3 = RVector3(0, 0, 0);
-
-    //時間
-    int time = 0;
-
-    Sprite testInstance;
-    float x1, x2, y1, y2;
-
-    //床
-    Object3d *tileObject;
-    UINT tiletex;
-
-    Object3d *animTestModel;
-    Object3d* testFbx;
-
-    //マウス確認変数
-    bool clicking = false;
-    bool clickTrigger = false;
-    bool clicked = false;
-    XMFLOAT2 mousePos;
-    XMFLOAT2 mouseVel;
-
-    //レンダリング結果描画
-
-    bool isadd = false;
-
-    //パーティクル
-    ParticleManager *particle1;
-    //パーティクルプロトタイプ
-    BomParticle *bomProto;
-    //パーティクル出現間隔
-    int pSpawnDelay = 5;
-    int PSPAWN_DELAY_TIME = 15;
-    //パーティクル出現数
-    int pSpawnNum = 1;
-    //パーティクルリソース
-
-    UINT ptex;
-
-    int adrate = 0;
-
-    int isControlColor1 = 0;
-
-    Player pl;
-
-    Enemy enemy;
-    AI_BehaviorTree enemyBehaviorTree;
-
-    BehaviorBaseNode* firstNode;
-
-    BehaviorBaseNode* approachNode;
-    ApproachingMoveAct* approachObject;
-    BehaviorBaseNode* retreatNode;
-    RetreatMoveAct* retreatObject;
-    BehaviorBaseNode* waitNode;
-    WaitAct* waitObject;
-
-    BehaviorEditor editor;
-
+    //円スプライト
+    Sprite circleSprite;
     
 };
 
